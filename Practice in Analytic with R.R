0d81c411c27cac0927006da39e2eae72f10f4ea8
unknown <MI00111@lmembers.com>
2017-01-18 12:39:43 +0900

    R을 이용한 데이터 처리 & 분석 실무

---
+++ Practice in Analytic with R.R
@@ -0,0 +1,16 @@
+## 유닛 테스팅을 위한 패키지
+install.packages("testthat")
+library(testthat)
+
+## expect 함수들
+
+#유닛 테스팅은 작성한 코드에 어떤 입력을 주었을 때 예상되는 출력이 실제로 반환되는지 확인하는 것을 기본으로 한다. 기댓값과 실제 반환 값을 비교하기 위해 사용하는 함수의 일부를 다음 표에 정리했다.
+
+a <- 1:3
+b <- 1:3
+expect_equal(a, b)
+expect_equivalent(a, b)
+names(a) <- c('a', 'b', 'c')
+expect_equal(a, b)       # 벡터에 부여한 이름이 다르므로 테스트가 실패
+expect_equivalent(a, b)  # 벡터에 부여한 이름을 무시하므로 테스트 성공
+
